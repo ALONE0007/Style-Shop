cd982272262e4df909fb4bfe9b607929b909bda2
MohamadMobin Rezaee <134555495+ALONE0007@users.noreply.github.com>
2025-02-13 12:30:54 +0330

    updating-FAQ

--- public/script.js
+++ public/script.js
@@ -1,25 +1,46 @@
-let navMenu = document.getElementById('navMenu')
-let closeNavBar = document.getElementById('closeNavBar')
-    
-closeNavBar.addEventListener("click",function(){
-    navMenu.style.top = '-900px'
-})
+let navMenu = document.getElementById("navMenu");
+let closeNavBar = document.getElementById("closeNavBar");
 
-document.querySelector('.custom-element').addEventListener('click', function(event) {
-    const element = document.querySelector('.custom-element');
+closeNavBar.addEventListener("click", function () {
+  navMenu.style.top = "-900px";
+});
+
+document
+  .querySelector(".custom-element")
+  .addEventListener("click", function (event) {
+    const element = document.querySelector(".custom-element");
     const rect = element.getBoundingClientRect();
-    
+
     // Define the approximate dimensions of the ::after pseudo-element
-    const afterWidth = 50;  // Adjust as needed
+    const afterWidth = 50; // Adjust as needed
     const afterHeight = 20; // Adjust as needed
     const afterLeft = rect.left + rect.width - afterWidth;
     const afterTop = rect.top + rect.height - afterHeight;
-  
+
     // Check if the click is within the bounds of the ::after element
-    if (event.clientX >= afterLeft && event.clientX <= afterLeft + afterWidth &&
-        event.clientY >= afterTop && event.clientY <= afterTop + afterHeight) {
-        navMenu.style.top = '0px'
-        
+    if (
+      event.clientX >= afterLeft &&
+      event.clientX <= afterLeft + afterWidth &&
+      event.clientY >= afterTop &&
+      event.clientY <= afterTop + afterHeight
+    ) {
+      navMenu.style.top = "0px";
     }
   });
-console.log(navMenu);
+
+//// faq ////
+
+let FAQElem = document.querySelectorAll("#faq");
+
+FAQElem.forEach(function (element) {
+  element.addEventListener("click", function () {
+    console.log(element);
+    FAQElem.forEach(function (item) {
+      if (item !== element) {
+        item.classList.remove("active-faq");
+      } else {
+        element.classList.toggle("active-faq");
+      }
+    });
+  });
+});
