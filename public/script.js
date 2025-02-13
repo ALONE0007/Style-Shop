a001f314144846ffead478acd87c654e43098b2e
MohamadMobin Rezaee <134555495+ALONE0007@users.noreply.github.com>
2025-02-13 22:17:56 +0330

    lazy

--- public/script.js
+++ public/script.js
@@ -43,3 +43,11 @@ FAQElem.forEach(function (element) {
     });
   });
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll("img");
+    images.forEach(function(img) {
+        img.setAttribute("loading", "lazy");
+    });
+});
